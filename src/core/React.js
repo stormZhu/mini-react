e2c6bff29c16fa4bb4660b1fc51a71af67034508
zhuyuqing <779724979@qq.com>
2025-11-23 16:06:07 +0800

    refactor(React): 提取函数组件和宿主组件的更新逻辑
    将performWorkOfUnit中的逻辑拆分为updateFunctionComponent和updateHostComponent两个独立函数
    提高代码可读性和可维护性

--- src/core/React.js
+++ src/core/React.js
@@ -106,25 +106,29 @@ function initChildren(fiber, children) {
   })
 }
 
+function updateFunctionComponent(fiber) {
+  const children = [fiber.type(fiber.props)]
+  initChildren(fiber, children)
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type))
+    // 2. 设置props
+    updateProps(dom, fiber.props)
+  }
+  initChildren(fiber, fiber.props.children)
+}
+
 function performWorkOfUnit(fiber) {
   const isFunctionComponent = typeof fiber.type === "function"
   if (!isFunctionComponent) {
-    // 1. 创建dom
-    if (!fiber.dom) {
-      const dom = (fiber.dom = createDom(fiber.type))
-      // fiber.parent.dom.append(dom) // 放入dom中，改为后面统一提交
-      // 2. 设置props
-      updateProps(dom, fiber.props)
-    }
+    updateHostComponent(fiber)
+  } else {
+    updateFunctionComponent(fiber)
   }
 
-  // 3. 转换链表，设置好指针
-  const children = isFunctionComponent
-    ? [fiber.type(fiber.props)]
-    : fiber.props.children
-  initChildren(fiber, children)
-
-  // 4. 返回下一个要执行的任务
+  // 返回下一个要执行的任务
   if (fiber.child) {
     return fiber.child
   }
